9ffbb399b85d9c749a7c3902e7773cfaa315539e
Karthik Nagarajan Sundar <86626195+karthiknagarajansundar@users.noreply.github.com>
2021-07-04 11:45:21 +0200

    Project 4

---
+++ Project 4/problem_A/main.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+
+xTaskHandle incTaskHandle;
+xTaskHandle decTaskHandle;
+xTaskHandle printFinalCounterTaskHandle;
+
+SemaphoreHandle_t incTaskDone;
+SemaphoreHandle_t decTaskDone;
+SemaphoreHandle_t Task;
+
+volatile int counter = 0;
+
+void incCounterTask(void *pvParameters)
+{
+    int i;
+    for(i = 0; i < 1e7; i++)
+    {
+    	xSemaphoreTake(Task, portMAX_DELAY);
+	counter = counter + 1;
+	xSemaphoreGive(Task);
+    }
+    xSemaphoreGive(incTaskDone); 
+    vTaskDelete(incTaskHandle);
+}
+
+void decCounterTask(void *pvParameters)
+{
+    int i;
+    for(i = 0; i < 1e7; i++ )
+    {
+    	xSemaphoreTake(Task, portMAX_DELAY);
+	counter = counter - 1;
+	xSemaphoreGive(Task);
+    }
+    xSemaphoreGive(decTaskDone); 
+    vTaskDelete(decTaskHandle);
+}
+
+void printFinalCounterTask(void *pvParameters)
+{
+    xSemaphoreTake(incTaskDone, portMAX_DELAY);
+    xSemaphoreTake(decTaskDone, portMAX_DELAY);
+    printf("Final value: %d\n", counter);
+    vTaskDelete(printFinalCounterTaskHandle);
+}
+
+int main(int argc, char **argv)
+{
+    incTaskDone = xSemaphoreCreateBinary();
+    decTaskDone = xSemaphoreCreateBinary();
+    Task = xSemaphoreCreateMutex();
+    xTaskCreate(incCounterTask, "Increase Counter Task", configMINIMAL_STACK_SIZE, NULL, 1, &incTaskHandle);
+    xTaskCreate(decCounterTask, "Decrease Counter Task", configMINIMAL_STACK_SIZE, NULL, 1, &decTaskHandle);
+    xTaskCreate(printFinalCounterTask, "Print Final Counter Task", configMINIMAL_STACK_SIZE, NULL, 1, &printFinalCounterTaskHandle);
+    vTaskStartScheduler();
+    for( ;; );
+}
